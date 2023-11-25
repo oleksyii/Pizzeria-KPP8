58dfc2c850c3eeab5cea0c99f77d00c8e38ef17a
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2023-11-25 10:03:50 +0200

    feat: PizzeriaController and packages

---
+++ src/main/java/code/example/demo2/controllers/PizzeriaController.java
@@ -0,0 +1,25 @@
+package code.example.demo2.controllers;
+
+public class PizzeriaController {
+    private PizzzeriaSimulatorViewModel simulatorViewModel;
+    private SettingsViewModel settingsView;
+
+    /**
+     * Must generate <b>ALL</b> the other classes in here.
+     * */
+    public PizzeriaController(){
+
+    }
+    public displayPizzaDetails(){
+
+    }
+    public displayClients(){
+
+    }
+    public GenerateClients(){
+
+    }
+    public StoreCook(id){
+
+    }
+}
