44a5725f83d44c9b4dcb72ea5a6f800b0d8efb93
Yevhen <epsonn537@gmail.com>
2023-11-26 12:34:54 +0200

    feat: add class animation

---
+++ src/main/java/com/example/demo2/MainPage/Animation.java
@@ -0,0 +1,33 @@
+package com.example.demo2.MainPage;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+public class Animation {
+
+    public void animateCook(Cook cook, double standStillDuration) {
+        TranslateTransition moveLeft = new TranslateTransition(Duration.seconds(2), cook);
+        moveLeft.setByX(-120);
+
+        TranslateTransition standStill = new TranslateTransition(Duration.seconds(standStillDuration), cook);
+        standStill.setByX(0);
+
+        TranslateTransition moveBack = new TranslateTransition(Duration.seconds(2), cook);
+        moveBack.setByX(120);
+
+        moveLeft.setOnFinished(event -> {
+            cook.changeState(CookState.AT_OVEN);
+            standStill.play();
+            standStill.setOnFinished(e -> {
+                moveBack.play();
+                moveBack.setOnFinished(f -> {
+                    // Оновлення стану тільки після закінчення анімації
+                    cook.changeState(CookState.AT_TABLE);
+                });
+            });
+        });
+
+        moveLeft.play();
+    }
+}
